602aa520d554eb5995065fb5650b473690ebb560
aishenhu <hit.huzhichao@gmail.com>
2012-11-08 08:03:11 +0800

    font stuff

--- js/effects.js
+++ js/effects.js
@@ -30,37 +30,45 @@ window.onload = function() {
 	// ctx.putImageData(imgData,0,0);
 	// 
 	(function() {
+		var pcanvas = document.getElementById('pcanvas');
+		var ctx = pcanvas.getContext("webgl") || pcanvas.getContext("experimental-webgl");
 		var $p = new Processing(document.getElementById('pcanvas'));
 		console.log($p);
 		var i = 0;
 		$p.draw = function() {
 			console.log('$p.draw called');
-			$p.rect(20,20,100,100);
-			//$p.size(200, 200, $p.OPENGL);
-			$p.noStroke();
-			$p.background(50);
-			$p.lights();
-			$p.translate($p.width / 2 + 30, $p.height / 2, 0);
-			$p.rotateX(-$p.PI / 6);
-			$p.rotateY($p.PI / 3 + 210 / $p.parseFloat($p.height) * $p.PI);
-			$p.box(45);
-			$p.translate(0, 0, -50 + i ++);
-			$p.box(30);
-			console.log('canvas:',$p.externals.canvas.getContext('2d'));
-			var context = $p.externals.canvas.getContext('2d');
-			context.drawText('Hello, world!');
+			//$p.rect(20,20,300,300);
+			// //$p.size(200, 200, $p.OPENGL);
+			// $p.noStroke();
+			// $p.pushMatrix();
+			// $p.background(50);
+			// $p.lights();
+			
+			// $p.translate($p.width / 2 + 30, $p.height / 2, 0);
+			// $p.rotateX(-$p.PI / 6);
+			// $p.rotateY($p.PI / 3 + 210 / $p.parseFloat($p.height) * $p.PI);
+			// $p.box(45);
+			// $p.translate(0, 0, -50 + i ++);
+			// $p.box(30);
+			// $p.popMatrix();
+			$p.background(0);
+			$p.fill(255,0,255);
+			$p.textSize(32);
+			$p.textAlign($p.CENTER,$p.CENTER);
+			$p.text('Hello g world 3 2 1',30,30,100,300);
 		}
 
 		$p.setup = function() {
 			console.log('$p.setup called');
-			$p.size(200, 200,$p.OPENGL);
+			$p.size(400, 400,$p.OPENGL);
 			$p.noStroke();
 			$p.frameRate( 60 );
-			$p.fill(255);
+			$p.background(0);
+			//$p.textFont($p.createFont('微软雅黑',32,true));
+			
 		}
 
 		$p.setup();
 
 	})();
-
 }
