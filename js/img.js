06108298a020f1cf456fe4dabe26eca6e0126013
aishenhu <hit.huzhichao@gmail.com>
2012-11-09 17:20:53 +0800

    use the real video to compare

--- js/img.js
+++ js/img.js
@@ -1,77 +1,47 @@
-Jx().$package('vp.img', function(J){
-	var $E = J.event;
-	var init = function(){
-		var base = new Image(),
-			baseCanvas = document.createElement('canvas'),
-			baseData,
-			screen = new Image(),
-			screenCanvas = document.createElement('canvas'),
-			screenData;
-		baseCanvas.id = 'baseCanvas';
-		screenCanvas.id = 'screenCanvas';
-		document.body.appendChild(baseCanvas);
-		document.body.appendChild(screenCanvas);
-		var ctx = baseCanvas.getContext('2d');
-		var ctx2 = screenCanvas.getContext('2d');
-		base.onload = function(){
-			baseCanvas.width = base.width;
-			baseCanvas.height = base.height;
-			ctx.drawImage(base,0,0);
-			baseData = ctx.getImageData(0,0, baseCanvas.width, baseCanvas.height);
-			onImageLoad();
-			//console.log(str);
+var ImageModule = {
+	init: function() {
+		var $ = function(id){
+			return document.getElementById(id)
 		}
-		base.setAttribute('src', 'assets/base.png');
-		screen.onload = function(){
-			screenCanvas.width = screen.width;
-			screenCanvas.height = screen.height;
-			ctx2.drawImage(screen,0,0);
-			screenData = ctx2.getImageData(0,0, screenCanvas.width, screenCanvas.height);
-			//console.log(str);
-			onImageLoad();
-		}
-		screen.setAttribute('src', 'assets/screen.png');
+		this.baseCanvas = $('base');
+		this.screenCanvas = $('screen');
+		this.compareCanvas = $('compare');
+	},
 
-		var loadCount = 0;
-		function onImageLoad(){
-			loadCount ++;
-			if(loadCount == 2){
-				processImageCompare();
+	processCompare: function(baseData, screenData) {
+		console.log('data length:', baseData.data.length, screenData.data.length);
+		var resData = screenData;
+		var step = 1;
+		var diffcount = 0;
+		var tStart = +new Date();
+		var range = 20;
+		for (var i = 0; i < baseData.data.length; i = i + 4 * step) {
+			if (Math.abs(baseData.data[i] - screenData.data[i]) > range || Math.abs(baseData.data[i] - screenData.data[i]) > range || Math.abs(baseData.data[i] - screenData.data[i]) > range) {
+				diffcount++;
+				resData.data[i] = 0;
+				resData.data[i + 1] = 0;
+				resData.data[i + 2] = 0;
+				resData.data[i + 3] = 255;
+			} else {
+				resData.data[i] = 255;
+				resData.data[i + 1] = 255;
+				resData.data[i + 2] = 255;
+				resData.data[i + 3] = 255;
 			}
 		}
-
-		function processImageCompare(){
-			console.log('data length:',baseData.data.length, screenData.data.length);
-			var step = 1;
-			var diffcount = 0;
-			var tStart = +new Date();
-			for(var i = 0; i < baseData.data.length; i = i +  4 * step){
-				var baseGray = (baseData.data[i]*0.299 + baseData.data[i+1] * 0.587 + baseData.data[i+2] * 0.114)/3;
-				baseData.data[i] = baseData.data[i+1] =baseData.data[i+2] = baseGray;
-				var screenGray = (screenData.data[i]*0.299 + screenData.data[i+1] * 0.587 + screenData.data[i+2] * 0.114)/3;
-				screenData.data[i] = screenData.data[i+1] =screenData.data[i+2] = screenGray;
-				var bd = baseGray + baseData.data[i+3];
-				var sd = screenGray + screenData.data[i+3];
-				if(bd - sd > 10 || sd - bd > 10){
-					diffcount ++;
-					screenData.data[i]= 0;
-					screenData.data[i+1]= 0;
-					screenData.data[i+2]= 0;
-					screenData.data[i+3]= 255;
-				}else{
-					screenData.data[i]= 255;
-					screenData.data[i+1]= 255;
-					screenData.data[i+2]= 255;
-					screenData.data[i+3]= 255;
-				}
-				
-			}
-			var tEnd = +new Date();
-			console.log('duration:',tStart, tEnd, tEnd-tStart);
-			console.log('diffcount:', diffcount, diffcount/4, Math.sqrt(diffcount/4));
-			ctx2.putImageData(screenData, 0,0);
+		var tEnd = +new Date();
+		console.log('duration:', tStart, tEnd, tEnd - tStart);
+		console.log('diffcount:', diffcount, diffcount / 4, Math.sqrt(diffcount / 4));
+		return {
+			data: resData,
+			duration: tEnd - tStart
 		}
+	},
+
+	start: function() {
+		var baseData = this.baseCanvas.getContext('2d').getImageData(0,0,this.baseCanvas.width, this.baseCanvas.height);
+		var screenData = this.screenCanvas.getContext('2d').getImageData(0,0,this.screenCanvas.width, this.screenCanvas.height);
+
+		return this.processCompare(baseData, screenData);
 	}
-
-	$E.onDomReady(init);
-});
+}
--- js/index.js
+++ js/index.js
@@ -1,33 +1,53 @@
 ;
 window.onload = function() {
-	var video = document.getElementById('video');
-	var canvas = document.getElementById('canvas');
-	if (navigator.getUserMedia) {
-		// opera users (hopefully everyone else at some point)
-		navigator.getUserMedia({
-			video: true,
-			audio: true
-		}, onSuccess, onError);
-	} else if (navigator.webkitGetUserMedia) {
-		// webkit users
-		navigator.webkitGetUserMedia({video:true}, onSuccess, onError);
-	}
+	var VideoModule = {
+		init: function() {
+			this.video = document.getElementById('video');
+		},
 
-	function onSuccess(stream) {
-		console.log(stream);
-		if(navigator.webkitGetUserMedia){
-			video.src = webkitURL.createObjectURL(stream);
-		}else{
-			video.src = stream;
+		start: function() {
+			if (navigator.getUserMedia) {
+				// opera users (hopefully everyone else at some point)
+				navigator.getUserMedia({
+					video: true,
+					audio: true
+				}, this.onSuccess, this.onError);
+			} else if (navigator.webkitGetUserMedia) {
+				// webkit users
+				navigator.webkitGetUserMedia({
+					video: true
+				}, this.onSuccess, this.onError);
+			}
+		},
+
+		onSuccess: function(stream) {
+			console.log(stream);
+			if (navigator.webkitGetUserMedia) {
+				video.src = webkitURL.createObjectURL(stream);
+			} else {
+				video.src = stream;
+			}
+			setTimeout(function() {
+				ImageModule.baseCanvas.getContext('2d').drawImage(video, 0, 0);
+				setInterval(function() {
+					ImageModule.screenCanvas.getContext('2d').drawImage(video, 0, 0);
+					var resData = ImageModule.start().data;
+					ImageModule.compareCanvas.getContext('2d').putImageData(resData, 0, 0);
+				}, 100);
+			}, 1000);
+
+		},
+
+		onError: function(error) {
+			console.log(error);
 		}
-		setInterval(function(){
-			canvas.width = parseInt(getComputedStyle(video).width);
-			canvas.height = parseInt(getComputedStyle(video).height);
-			canvas.getContext('2d').drawImage(video, 0, 0);
-		},20);
 	}
-
-	function onError(error) {
-		console.log(error);
-	}
+	// var worker = new Worker('js/img.js');
+	ImageModule.init();
+	VideoModule.init();
+	VideoModule.start();
+	// ImageModule.baseCanvas.getContext('2d').drawImage(document.getElementById('baseImg'),0,0);
+	// ImageModule.screenCanvas.getContext('2d').drawImage(document.getElementById('screenImg'),0,0);
+	// var resData = ImageModule.start().data;
+	// ImageModule.compareCanvas.getContext('2d').putImageData(resData, 0,0);
 }
