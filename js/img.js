c20cb01f1a0c366a5f0f6ab4effc55bf110ec5e5
aishenhu <hit.huzhichao@gmail.com>
2012-11-10 13:16:57 +0800

    test

--- js/img.js
+++ js/img.js
@@ -6,9 +6,78 @@ var ImageModule = {
 		this.baseCanvas = $('base');
 		this.screenCanvas = $('screen');
 		this.compareCanvas = $('compare');
+		this.grayCanvas = $('gray');
+	},
+
+	getImageData: function(canvas){
+		return canvas.getContext('2d').getImageData(0,0,canvas.width,canvas.height);
+	},
+
+	getGrayImageData: function(imageData){
+		for(var i = 0; i < imageData.data.length; i = i + 4){
+			var grayData = imageData.data[i] * 0.299 + imageData.data[i]*0.587 + imageData.data[i]* 0.114;
+			imageData[i] = grayData;
+			imageData[i+1] = grayData;
+			imageData[i+2] = grayData;
+		}
+		return imageData;
 	},
 
 	processCompare: function(baseData, screenData) {
+		console.log('data length:', baseData.data.length, screenData.data.length);
+		var resData = screenData;
+		var step = 1;
+		var diffcount = 0;
+		var tStart = +new Date();
+		var range = 20;
+		baseData = this.getGrayImageData(baseData);
+		screenData = this.getGrayImageData(screenData);
+		for (var i = 0; i < baseData.data.length; i = i + 4 * step) {
+			if (Math.abs(baseData.data[i] - screenData.data[i]) > range || Math.abs(baseData.data[i] - screenData.data[i]) > range || Math.abs(baseData.data[i] - screenData.data[i]) > range) {
+				diffcount++;
+				resData.data[i] = 0;
+				resData.data[i + 1] = 0;
+				resData.data[i + 2] = 0;
+				resData.data[i + 3] = 255;
+			} else {
+				resData.data[i] = 255;
+				resData.data[i + 1] = 255;
+				resData.data[i + 2] = 255;
+				resData.data[i + 3] = 255;
+			}
+		}
+		var tEnd = +new Date();
+		console.log('duration:', tStart, tEnd, tEnd - tStart);
+		console.log('diffcount:', diffcount, diffcount / 4, Math.sqrt(diffcount / 4));
+		return {
+			data: resData,
+			duration: tEnd - tStart
+		}
+	},
+
+	differenceAccuracy: function(target, data1, data2) {
+		if (data1.length != data2.length) return null;
+		var i = 0;
+		function fastAbs(value) {
+			// funky bitwise, equal Math.abs
+			return (value ^ (value >> 31)) - (value >> 31);
+		}
+
+		function threshold(value) {
+			return (value > 0x15) ? 0xFF : 0;
+		}
+		while (i < (data1.length * 0.25)) {
+			var average1 = (data1[4*i] + data1[4*i+1] + data1[4*i+2]) / 3;
+			var average2 = (data2[4*i] + data2[4*i+1] + data2[4*i+2]) / 3;
+			var diff = threshold(fastAbs(average1 - average2));
+			target[4*i] = diff;
+			target[4*i+1] = diff;
+			target[4*i+2] = diff;
+			target[4*i+3] = 0xFF;
+			++i;
+		}
+	},
+	compare: function(baseData, screenData) {
 		console.log('data length:', baseData.data.length, screenData.data.length);
 		var resData = screenData;
 		var step = 1;
--- js/index.js
+++ js/index.js
@@ -1,18 +1,51 @@
 ;
 window.onload = function() {
+	/**
+	 * 创建帧动画对象，传入的函数，将在下一帧绘制时执行，所以函数只执行
+	 * 一次
+	 * 在不支持requestAnimationFrame的情况下，使用定时器模拟，fps 60
+	 * 使用示例：
+	 * @example
+	 *         var raf = requestAnimationFrame;
+	 *         raf(function(){
+	 *              //处理代码
+	 *         }, {});
+	 * 
+	 */
+	var requestAnimationFrame = window.requestAnimationFrame 		|| 
+								window.webkitRequestAnimationFrame  || 
+								window.mozRequestAnimationFrame 	|| 
+								window.oRequestAnimationFrame		|| 
+								window.msRequestAnimationFrame 		||
+								function(callback, element) {
+									window.setTimeout(callback, 1000 / 60);
+								};
+
+	/**
+	 * 通过帧动画对象循环调用func
+	 * @param {function} func 按帧循环调用的函数
+	 */
+	var rafLoop = function(func) {
+		var raf = requestAnimationFrame;
+		raf(function() {
+			func();
+			raf(arguments.callee, {});
+		}, {});
+	}
+
 	var VideoModule = {
 		init: function() {
 			this.video = document.getElementById('video');
 		},
 
 		start: function() {
-			if (navigator.getUserMedia) {
+			if(navigator.getUserMedia) {
 				// opera users (hopefully everyone else at some point)
 				navigator.getUserMedia({
 					video: true,
 					audio: true
 				}, this.onSuccess, this.onError);
-			} else if (navigator.webkitGetUserMedia) {
+			} else if(navigator.webkitGetUserMedia) {
 				// webkit users
 				navigator.webkitGetUserMedia({
 					video: true
@@ -22,20 +55,41 @@ window.onload = function() {
 
 		onSuccess: function(stream) {
 			console.log(stream);
-			if (navigator.webkitGetUserMedia) {
+			if(navigator.webkitGetUserMedia) {
 				video.src = webkitURL.createObjectURL(stream);
 			} else {
 				video.src = stream;
 			}
-			setTimeout(function() {
+			// ImageModule.baseCanvas.getContext('2d').drawImage(video, 0, 0);
+			// setTimeout(function() {
+			// 	setInterval(function() {
+			// 		ImageModule.screenCanvas.getContext('2d').drawImage(video, 0, 0);
+			// 		var screenImageData = ImageModule.getImageData(ImageModule.screenCanvas);
+			// 		var grayImageData = ImageModule.getGrayImageData(screenImageData);
+			// 		var resData = ImageModule.start().data;
+			// 		ImageModule.grayCanvas.getContext('2d').putImageData(grayImageData, 0, 0);
+			// 		ImageModule.compareCanvas.getContext('2d').putImageData(resData, 0, 0);
+			// 	}, 100);
+			// }, 2000);
+			var lastImageData = 0;
+			var frameCount = 0;
+			rafLoop(function(){
+				frameCount ++;
 				ImageModule.baseCanvas.getContext('2d').drawImage(video, 0, 0);
-				setInterval(function() {
-					ImageModule.screenCanvas.getContext('2d').drawImage(video, 0, 0);
-					var resData = ImageModule.start().data;
-					ImageModule.compareCanvas.getContext('2d').putImageData(resData, 0, 0);
-				}, 100);
-			}, 1000);
-
+				var width = ImageModule.baseCanvas.width;
+				var height = ImageModule.baseCanvas.height;
+				var curImageData = ImageModule.baseCanvas.getContext('2d').getImageData(0,0,width,height);
+				if(!lastImageData){
+					lastImageData = curImageData;
+				}
+				var resData = ImageModule.baseCanvas.getContext('2d').createImageData(width,height);
+				ImageModule.differenceAccuracy(resData.data, lastImageData.data, curImageData.data);
+				ImageModule.compareCanvas.getContext('2d').putImageData(resData,0,0);
+				
+				if(frameCount % 10 == 1){
+					lastImageData = curImageData;
+				}
+			});
 		},
 
 		onError: function(error) {
